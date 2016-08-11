c84d7a5cede00fa87d1c6379cb81cfa1bd7a7c18
Mauricio Bonetti <bonetti.mauricio@gmail.com>
2016-08-11 20:18:54 +0000

    Index page

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
+app.use(express.static("public"));
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log('ImageSearch app listening on port 8080!');
